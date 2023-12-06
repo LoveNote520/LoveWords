1e9f1ce2b41d598a81761539d5763f951cbdbc02
acsweet <jurelxc@163.com>
2023-12-06 18:59:24 +0800

    优雅

--- lib/algorithm_every_day/list/linked_list.dart
+++ lib/algorithm_every_day/list/linked_list.dart
@@ -19,9 +19,9 @@
 /// 移除尾结点: void removeLast()
 /// 移除首结点: void removeFirst()
 ///
-library;
 
-void main() {
+
+void main(){
   SLinkedList<int> sList = SLinkedList();
   sList.addAt(0, 1);
   sList.addAt(1, 2);
@@ -30,12 +30,12 @@ void main() {
   print(sList); // 3->4->1->2
   print(sList.size); // 4
 
-  // sList.removeAt(2);
-  // print(sList); // 3->4->2
-  //
-  // sList.set(2, 1);
-  // print(sList); // 3->4->1
-  // print(sList.get(2)); //
+  sList.removeAt(2);
+  print(sList); // 3->4->2
+
+  sList.set(2,1);
+  print(sList); // 3->4->1
+  print(sList.get(2)); //1
 
   sList.addFirst(5);
   print(sList); // 5->3->4->1
@@ -45,11 +45,29 @@ void main() {
 
   sList.removeFirst();
   print(sList); // 3->4->1->6
-
+  print(sList.size);//4
   sList.removeLast();
   print(sList); // 3->4->1
-}
 
+   sList.addAt(2,4);
+   print(sList); // 3->4->4->1
+
+   int firstIndex = sList.firstIndexOf(4);
+   print(firstIndex); // 1
+
+  int lastIndex = sList.lastIndexOf(4);
+  print(lastIndex); // 2
+
+   bool contains = sList.contains(3);
+   print(contains); // 1
+
+   sList.addArrayAt(3, [999,998]);
+   print(sList); // 3->4->4->999->998->1
+  //
+  List<int> ret = sList.removeBetween(2, 4);
+  print(ret); // [4, 999, 998]
+  print(sList); // 3->4->1
+}
 class SLinkedList<T> {
   final SListNode<T> _head = SListNode<T>(null);
 
@@ -125,7 +143,7 @@ class SLinkedList<T> {
   }
 
   void removeLast() {
-    removeAt(_size);
+    removeAt(_size-1);
   }
 
   void removeFirst() {
@@ -137,17 +155,18 @@ class SLinkedList<T> {
   /// 对应元素尾索引 : int lastIndexOf(T value) 最后一个出现的索引,不存在返回 -1
   /// 是否包含元素: bool contains(T value)
 
-  int _indexOf(T value, {int start = 0}) {
-    SListNode? cur = _head.next;
-    int index = start;
-
+  int _indexOf(T value, {bool start = true}) {
+    int result = -1;
+    int counter = 0;
+    SListNode<T>? cur = _head.next;
     while (cur != null) {
-      if (cur.val == value) return index;
+      if (cur.val == value) {
+        result = counter;
+        if (start) break;
+      }
       cur = cur.next;
-      index++;
     }
-
-    return -1;
+    return result;
   }
 
   int firstIndexOf(T value) {
@@ -155,19 +174,34 @@ class SLinkedList<T> {
   }
 
   int lastIndexOf(T value) {
-    int last = -1;
-    int index = _indexOf(value);
 
-    while (index != -1) {
-      last = index;
-      index = _indexOf(value, start: last + 1);
-    }
-
-    return last;
+    return _indexOf(value,start:false);
   }
 
   bool contains(T value) {
     return firstIndexOf(value) != -1;
+  }
+
+  /// [2023.12.06]
+  /// 指定位置添加多个节点 : void addArrayAt(int index, List<T> values)
+  /// 移除范围内的节点: List<T> removeBetween(int start, int end)
+  ///
+  ///
+  void addArrayAt(int index, List<T> values) {
+    for (int i = 0; i < values.length; i++) {
+      addAt(index + i, values[i]);
+    }
+    _size = _size + values.length;
+  }
+
+  List<T> removeBetween(int start, int end) {
+    List<T> removeValue = [];
+    for (int i = end - start; i >= 0; i--) {
+      removeValue.add(get(start) as T);
+      removeAt(start);
+    }
+    _size = _size - (end - start);
+    return removeValue;
   }
 
   @override
